e0b1d8b1dc5b7d6ef832cc92af13471fe0183b30
carmensandiego <lane.eyestone@gmail.com>
2015-10-24 06:28:48 -0400

    Lexical Scoping initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## MakeCacheMatrix and cacheSolve are used to efficiently return
+## the inverse of a matrix and cache the output without recalculating in 
+## the global environment
 
-## Write a short comment describing this function
+## MakeCacheMatrix returns a list of pointers to the global environment
+## which contains functions that grant access to the environment in which
+## the functions were defined demonstrating lexical scoping
 
 makeCacheMatrix <- function(x = matrix()) {
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) i <<- solve
+        getinv <- function() i
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns the inverse of a matrix using the functions 
+## contained within the list of pointers returned by makeCacheMatrix
+## it checks to see if the inverse was already calculated and if so,
+## it will return the cached inverse, rather than recalculate it
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        i <- x$getinv()
+        if(!is.null(i)){
+                message("getting inverse of matrix")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinv(i)
+        i
 }
